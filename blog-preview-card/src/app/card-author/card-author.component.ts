5484e384d074ce0f7bc862d822c6a9f37cbd0adc
Kellen James <8484636@ups.com>
2024-10-29 16:04:38 -0400

    chore: generate angular components to describe the UI

---
+++ blog-preview-card/src/app/card-author/card-author.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-author',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-author.component.html',
+  styleUrl: './card-author.component.scss'
+})
+export class CardAuthorComponent {
+
+}
